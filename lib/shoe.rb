aa7590e30704faf764e506f4fd490cee0fb13515
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-10-16 20:44:05 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,19 +3,14 @@ class Shoe
   attr_reader :brand
   
   BRANDS = []
-  
+
   def initialize(brand)
     @brand = brand
-  end
-  
-   def brand=(brand)
-    @brand = brand 
     BRANDS << brand unless BRANDS.include?(brand)
-  end 
+  end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 end
-adidas = Shoe.new("adidas")
